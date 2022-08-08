7067568c72699d9cc11552185f9c43ee5bf4cc48
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2022-08-08 15:07:39 +0200

    Added extrapolation operator

--- src/motion_parameter_utils.jl
+++ src/motion_parameter_utils.jl
@@ -1,6 +1,6 @@
 # Motion parameter utilities
 
-export derivative1d_linop, derivative1d_motionpars_linop, interpolation1d_linop, interpolation1d_motionpars_linop
+export derivative1d_linop, derivative1d_motionpars_linop, interpolation1d_linop, interpolation1d_motionpars_linop, extrapolate_motionpars_linop
 
 function derivative1d_linop(t::AbstractVector{T}, order::Integer) where {T<:Real}
     (order != 1) && (order != 2) && error("Order not supported (only 1 or 2)")
@@ -80,3 +80,34 @@ interpolation1d_motionpars_linop(t::AbstractVector{T}, ti::NTuple{6,AbstractVect
 interpolation1d_motionpars_linop(t::NTuple{6,AbstractVector{T}}, ti::AbstractVector{T}; interp::Symbol=:linear) where {T<:Real} = interpolation1d_motionpars_linop(t, (ti,ti,ti,ti,ti,ti); interp=interp)
 
 interpolation1d_motionpars_linop(t::AbstractVector{T}, ti::AbstractVector{T}; interp::Symbol=:linear) where {T<:Real} = interpolation1d_motionpars_linop((t,t,t,t,t,t), (ti,ti,ti,ti,ti,ti); interp=interp)
+
+function extrapolate_motionpars_linop(coord_q::AbstractArray{T,2}, coord::AbstractArray{T,2}; kernel_size::Integer=1, dist_fcn::Function=r2->exp.(-r2/2), all_pars::Bool=false) where {T<:Real}
+    nt = size(coord,1); nt_q = size(coord_q,1)
+    Is = repeat(reshape(1:nt,1,:); outer=(kernel_size,1))
+    Js = Array{Integer,2}(undef,kernel_size,nt)
+    Vs = Array{T,2}(undef,kernel_size,nt)
+    @inbounds for i = 1:nt
+        dist2_i = sum((coord[i:i,:].-coord_q).^2; dims=2)[:,1]
+        Js[:,i], dist2_min = mink(dist2_i, kernel_size)
+        Vs[:,i] = dist_fcn(dist2_min)
+        # (dist2_min[1] == 0) && (Vs[2:end,i] .= 0)
+        # i_cutoff = findfirst(cumsum(Vs[:,i]) .> 1)
+        # ~isnothing(i_cutoff) && (Vs[i_cutoff:end,i] .= 0)
+        Vs[:,i] ./= sum(Vs[:,i])
+    end
+    A = sparse(vec(Is), vec(Js), vec(Vs), nt, nt_q)
+    all_pars ? (return kron(Matrix{T}(I, 6, 6), A)) : (return A)
+end
+
+function extrapolate_motionpars_linop(n::NTuple{2,Integer}, idx_q::AbstractVector{<:Integer}, idx::Union{Nothing,AbstractVector{<:Integer}}; T::DataType=Float32, kernel_size::Integer=1, dist_fcn::Function=r2->exp.(-r2/2), all_pars::Bool=false)
+    isnothing(idx) && (idx = 1:prod(n))
+    x = repeat(reshape(T.(1:n[1]),:,1); outer=(1,n[2]))
+    y = repeat(reshape(T.(1:n[2]),1,:); outer=(n[1],1))
+    coord = [vec(x) vec(y)]
+    return extrapolate_motionpars_linop(coord[idx_q,:], coord[idx,:]; kernel_size=kernel_size, dist_fcn=dist_fcn, all_pars=all_pars)
+end
+
+function mink(a::AbstractArray{T}, k::Integer) where {T<:Real}
+    b = partialsortperm(a, 1:k)
+    return b, a[b]
+end
--- test/test_motion_parameter_utils.jl
+++ test/test_motion_parameter_utils.jl
@@ -1,14 +1,31 @@
-using UtilitiesForMRI, LinearAlgebra, Test
+using UtilitiesForMRI, LinearAlgebra, Test, Random
+Random.seed!(123)
 
-nt = 256
-t = Float64.(1:nt)
-D = derivative1d_motionpars_linop(t, 2; pars=(true,false,true,false,true,true))
-θ = zeros(Float64, nt, 6); θ[129-30:129+30,:] .= 1; θ[1:10,1] .= 1; θ[end-9:end,1] .= 1
-Dθ = reshape(D*vec(θ), :, 6)
+# nt = 256
+# t = Float64.(1:nt)
+# D = derivative1d_motionpars_linop(t, 2; pars=(true,false,true,false,true,true))
+# θ = zeros(Float64, nt, 6); θ[129-30:129+30,:] .= 1; θ[1:10,1] .= 1; θ[end-9:end,1] .= 1
+# Dθ = reshape(D*vec(θ), :, 6)
 
-nt = 256+1
-t = Float64.(0:nt-1)
-ti = Float64.(0:2:nt-1); nti = length(ti)
-Itp = interpolation1d_motionpars_linop((ti,ti,t,ti,ti,t), t)
-θ = randn(Float64, 4*nti+2*nt)
-Iθ = reshape(Itp*θ, nt, 6)
+# nt = 256+1
+# t = Float64.(0:nt-1)
+# ti = Float64.(0:2:nt-1); nti = length(ti)
+# Itp = interpolation1d_motionpars_linop((ti,ti,t,ti,ti,t), t)
+# θ = randn(Float64, 4*nti+2*nt)
+# Iθ = reshape(Itp*θ, nt, 6)
+
+# Extrapolation linear operator
+fov = (1f0, 2f0, 3f0)
+n = (256, 256, 256)
+X = spatial_geometry(fov, n)
+phase_encoding_dims = (2, 3); readout = 1
+pe_subs = randperm(prod(n[[phase_encoding_dims...]]))[1:6000]
+I = extrapolate_motionpars_linop(n[[phase_encoding_dims...]], pe_subs, nothing; T=Float32, kernel_size=1, dist_fcn=r2->exp.(-r2/(2*1^2)))
+d = randn(Float32, length(pe_subs))
+d_ = reshape(I*d, n[[phase_encoding_dims...]])
+# using PyPlot
+# imshow(d_)
+
+# Consistency w/ all-parameter extrapolation operator
+I6 = extrapolate_motionpars_linop(n[[phase_encoding_dims...]], pe_subs, nothing; T=Float32, kernel_size=10, dist_fcn=r2->exp.(-r2/(2*2^2)), all_pars=true)
+@test vec(repeat(I*d; outer=(1,6))) ≈ I6*vec(repeat(d; outer=(1,6))) rtol=1f-6
